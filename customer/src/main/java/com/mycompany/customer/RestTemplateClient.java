2df5fa7e10ab8bedbda6a70cc5c146a6d1ac5560
Sebastian Osadnik <sebastian.osadnikk@gmail.com>
2021-12-22 14:50:51 +0100

    adding swagger

--- customer/src/main/java/com/mycompany/customer/RestTemplateClient.java
+++ customer/src/main/java/com/mycompany/customer/RestTemplateClient.java
@@ -3,6 +3,9 @@ package com.mycompany.customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class RestTemplateClient {
@@ -12,5 +15,11 @@ public class RestTemplateClient {
         return new RestTemplate();
     }
 
-
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build();
+    }
 }
